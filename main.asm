1d7016223b3c6d186a10bc3f4a2d36282aaab2cc
0x0015 <64482595+0x0015@users.noreply.github.com>
2024-09-17 20:26:10 -0400

    initial commit

---
+++ main.asm
@@ -0,0 +1,17 @@
+section .data
+message: db "Hello World!\n", 0
+
+global _start
+
+	section .text
+
+_start:
+	mov rax, 1 ;write syscall
+	mov rdi, 1 ;file handel 1 = stdout
+	mov rsi, message
+	mov rdx, 13 ;message length
+	syscall
+
+	mov rax, 60 ;syscall for exit
+	xor rdi, rdi
+	syscall
---
+++ sdl3.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL3/SDL.h>
+
+struct vec2{
+	float x;
+	float y;
+};
+
+int main(){
+	SDL_Event event;
+	bool running = true;
+	vec2 resolution = {720, 480};
+	if(!SDL_Init(SDL_INIT_VIDEO)){
+		printf("Failed to init SDL3 Video: %s\n",SDL_GetError());
+		return -1;
+	}
+	SDL_Window* window = SDL_CreateWindow("springSim Window", resolution.x, resolution.y, 0b0);
+	if(window == nullptr){
+		printf("Failed to create SDL3 Window: %s\n",SDL_GetError());
+		return;
+	}
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+	if(renderer == nullptr){
+		printf("Failed to create SDL3 Renderer: %s\n",SDL_GetError());
+		return;
+	}
+	float lastFrameTime = SDL_GetTicks() / 1000.0f;
+
+	while(running){
+		while(SDL_PollEvent(&event)){
+			if(event.type == SDL_EVENT_QUIT)
+				running = false;
+			if(event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_ESCAPE)
+				running = false;
+		}
+
+		float t = SDL_GetTicks() / 1000.0f;
+		float dt = t - lastFrameTime;
+		lastFrameTime = t;
+
+		SDL_SetRenderDrawColorFloat(renderer, 0.0f, 0.0f, 0.0f, 1.0f);
+		SDL_RenderClear(renderer);
+
+		SDL_SetRenderDrawColorFloat(renderer, 1.0f, 0.0f, 0.0f, 1.0f);
+		SDL_FRect rect{0, 0, 100, 100};
+		SDL_RenderRect(renderer, &rect);
+
+		SDL_RenderPresent(renderer);
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
